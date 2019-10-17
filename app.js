f9802c4ed3a768495a965d1a4dfd7cc0dad6761c
Mausumi Patra <someepatra@gmail.com>
2019-10-17 09:04:18 -0400

    updated

--- app.js
+++ app.js
@@ -1,119 +1,94 @@
 $(() => {
-  console.log("linked");
-
   //front screen showing all news
   const endpoint =
     "https://newsapi.org/v2/everything?q=news&apiKey=63fa7350c3b0494b91b7f042d7e9b001";
   console.log(endpoint);
-  $.ajax({
-    url: endpoint
-  }).then(data => {
-    console.log(data);
-    let text;
-    let $showData = $("#show-data");
-    for (i = 1; i < data.articles.length; i++) {
-      text += data.articles[i].title + "<br>";
-    }
-    $showData.append(text);
-  });
-});
-const $container = $(".container");
-
-// when Sports is clicked
-$("#sports").on("click", event => {
-  const endpoint =
-    "https://newsapi.org/v2/everything?q=sports&apiKey=63fa7350c3b0494b91b7f042d7e9b001";
 
   $.ajax({
     url: endpoint
   }).then(data => {
     console.log(data);
+    let $leftShowData = $(".left-container");
+    let $rightShowData = $(".right-container");
 
-    for (let i = 0; i < 3; i++) {
-      const $author = $("<h1>").text(data.articles[i].author);
-      const $title = $("<h2>").text(data.articles[i].title);
+    console.log(data.articles.length);
+    for (i = 0; i < 20; i++) {
+      const $title = $("<h6>").text(data.articles[i].title);
+      let $img = $("<img>").attr("src", data.articles[i].urlToImage);
+      let $url = $("<a>").attr("href", data.articles[i].url);
+      $url.text("read more....");
 
-      // const $img = $("<img>").attr("src", data.articles[i].urlToImage);
-
-      $("#sports").append($title);
-      //$("#sports").append($img);
+      if (i < 10) {
+        $leftShowData.append($img);
+        $leftShowData.append($title);
+        $leftShowData.append($url);
+      } else {
+        $rightShowData.append($img);
+        $rightShowData.append($title);
+        $rightShowData.append($url);
+      }
     }
   });
 });
-
-// when Politics is clicked
-$("#politics").on("click", event => {
-  const endpoint =
-    "https://newsapi.org/v2/everything?q=politics&apiKey=63fa7350c3b0494b91b7f042d7e9b001";
-
-  $.ajax({
-    url: endpoint
-  }).then(data => {
-    console.log(data);
-
-    for (let i = 0; i < 3; i++) {
-      const $author = $("<h1>").text(data.articles[i].author);
-      const $title = $("<h2>").text(data.articles[i].title);
-      //const $img = $("<img>").attr("src", data.articles[i].urlToImage);
-
-      $("#politics").append($title);
-      //$("#politics").append($img);
-    }
-  });
-});
-
-// when Movies is clicked
-$("#movies").on("click", event => {
-  const endpoint =
-    "https://newsapi.org/v2/everything?q=movies&apiKey=63fa7350c3b0494b91b7f042d7e9b001";
+const option = ["current", "sports", "politics", "movies", "weather"];
+// default values
+let whenSelect = "movies";
+let currentIndex = 3;
+// fetch data
+const fetchData = topic => {
+  const endpoint = `https://newsapi.org/v2/everything?q=${topic}&apiKey=63fa7350c3b0494b91b7f042d7e9b001`;
 
   $.ajax({
     url: endpoint
   }).then(data => {
     console.log(data);
-    for (let i = 0; i < 3; i++) {
-      const $title = $("<h1>").text(data.articles[i].title);
+    $("#list-data").empty();
+    for (let i = 0; i < 7; i++) {
+      const $title = $("<h6>").text(data.articles[i].title);
       const $img = $("<img>").attr("src", data.articles[i].urlToImage);
-
-      $("#movies").append($title);
-      $("#movies").append($img);
+      let $url = $("<a>").attr("href", data.articles[i].url);
+      $url.text("read more....");
+      $("#list-data").append($img);
+      $("#list-data").append($title);
+      $("#list-data").append($url);
     }
   });
-});
+};
+// when Buttons  clicked
+$(".option").on("click", event => {
+  whenSelect = event.currentTarget.id;
 
-//when Weather is clicked
-$("#weather").on("click", event => {
-  const endpoint =
-    "https://newsapi.org/v2/everything?q=weather&apiKey=63fa7350c3b0494b91b7f042d7e9b001";
-
-  $.ajax({
-    url: endpoint
-  }).then(data => {
-    console.log(data);
-    for (let i = 0; i < 3; i++) {
-      const $title = $("<h1>").text(data.articles[i].title);
-      const $img = $("<img>").attr("src", data.articles[i].urlToImage);
-
-      $("#weather").append($title);
-      $("#weather").append($img);
+  for (let i = 0; i < option.length; i++) {
+    if (option[i] === event.currentTarget.id) {
+      currentIndex = i;
+      //console.log(currentIndex);
     }
-  });
+  }
+  fetchData(whenSelect);
 });
-//when Current is clicked
-$("#current").on("click", event => {
-  const endpoint =
-    "https://newsapi.org/v2/everything?q=current&apiKey=63fa7350c3b0494b91b7f042d7e9b001";
-
-  $.ajax({
-    url: endpoint
-  }).then(data => {
-    console.log(data);
-    for (let i = 0; i < 3; i++) {
-      const $title = $("<h1>").text(data.articles[i].title);
-      const $img = $("<img>").attr("src", data.articles[i].urlToImage);
-
-      $("#current").append($title);
-      $("#current").append($img);
+// when direction buttons clicked
+//currentIndex = 0;
+$(".direction-buttons").on("click", event => {
+  if (event.currentTarget.id === "next") {
+    if (currentIndex < option.length - 1) {
+      currentIndex++;
+    } else {
+      currentIndex = 0;
+      whenSelect = option[0];
     }
-  });
+    whenSelect = option[currentIndex];
+    console.log(whenSelect);
+  } else {
+    if (currentIndex > 0) {
+      currentIndex--;
+    } else {
+      currentIndex = option.length - 1;
+      whenSelect = option[option.length - 1];
+    }
+    whenSelect = option[currentIndex];
+    console.log(whenSelect);
+  }
+
+  fetchData(whenSelect);
 });
+fetchData(whenSelect);
